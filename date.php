8ca52c186fac5fe3eb558a02820a7d16be772d90
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-14 17:12:44 +0200

    feat add bonus 1 and 2

--- date.php
+++ date.php
@@ -1,22 +1,23 @@
 <?php
 
-
-$babylon_type = new Type('Drammatico','Comico');
+// istanza che prende dati da class Type
+$babylon_type = new Type('Drammatico','Comico','Musica');
+// istanza che prende i dati da class Movie
 $babylon = new Movie('Babylon', '2023', 'Damien Chazelle', $babylon_type);
 
 $creed_type = new Type('Azione','Box');
 $creed = new Movie('Creed', '2020', 'Ryab Coogle', $creed_type);
 
-$avengers_type = new Type('Azione', 'Supereroi');
+$avengers_type = new Type('Azione', 'Supereroi','Fantasy');
 $avengers = new Movie('Avengers: Endgame', '2019', 'Anthony e Joe Russo', $avengers_type);
 
-$inception_type = new Type('Azione', 'Thriller');
+$inception_type = new Type('Azione','Fantasy', 'Thriller','Mistero');
 $inception = new Movie('Inception', '2010', 'Christopher Nolan', $inception_type);
 
 $interstellar_type = new Type('Fantascienza', 'Avventura');
 $interstellar = new Movie('Interstellar', '2014', 'Christopher Nolan', $interstellar_type);
 
-$matrix_type = new Type('Azione', 'Sci-Fi');
+$matrix_type = new Type('Azione', 'Sci-Fi','Fantasy','Mistero');
 $matrix = new Movie('The Matrix', '1999', 'The Wachowskis', $matrix_type);
 
 $mad_max_type = new Type('Azione', 'Apocalittico');
--- index.php
+++ index.php
@@ -29,8 +29,41 @@ $movies = [
 </head>
 
 <body>
-    <?php echo $babylon->getFullName(); ?> <br>
-    <?php echo $creed->getFullName(); ?>
+    <h2>FILM</h2>
+    <div class="container">
+        <!-- forech per stampare tutti i dati di tutti i film -->
+        <?php foreach ($movies as $movie) {   ?>
+            <div class="items">
+                <h5>Titolo: <?php echo $movie->title ?></h5>
+                <p>Anno: <?php echo $movie->years ?></p>
+                <p>Registra:<?php echo $movie->moviedirector ?></p>
+                
+                <p>Genere:</p>
+                <ul>
+                    <?php
+                    // verificare se ci sono generi e stamparli
+                    if ($movie->type->type1 !== null) {
+                        echo '<li>' . $movie->type->type1 . '</li>';
+                    }
+                    if ($movie->type->type2 !== null) {
+                        echo '<li>' . $movie->type->type2 . '</li>';
+                    }
+                    if ($movie->type->type3 !== null) {
+                        echo '<li>' . $movie->type->type3 . '</li>';
+                    }
+                    if ($movie->type->type4 !== null) {
+                        echo '<li>' . $movie->type->type4 . '</li>';
+                    }
+                    ?>
+                    
+                     <!-- <li>  <?php echo $movie->type->type1 ?></li> 
+                     <li>  <?php echo $movie->type->type2 ?></li> 
+                     <li>  <?php echo $movie?->type->type3 ?></li> 
+                     <li>  <?php echo $movie?->type->type4 ?></li>  -->
+                </ul>
+            </div>
+        <?php } ?>
+    </div>
     <script type="text/javascript" src="js/script.js"></script>
 </body>
 
